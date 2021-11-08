26ae1a90891ba9cf18b5a727fecbbe4c7a9f937c
David <0x46697265@gmail.com>
2021-11-08 04:04:36 -0700

    v5

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import {Calculator} from "/js/calc.js";
+
+new Calculator({
+	elements: {
+		original: document.getElementById("table1"),
+		new: document.getElementById("table2"),
+		final: false,
+		calculate: document.getElementById("calculate"),
+	}
+});
