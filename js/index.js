551d82fe5449dea7532fdc64e8ed743b43441da5
David <0x46697265@gmail.com>
2021-11-08 10:01:13 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
+import {Calculator} from "/js/calc.js";
 "use strict";
 
-var _calc = require("/js/calc.js");
-
-new _calc.Calculator({
-  elements: {
-    original: document.getElementById("table1"),
-    new: document.getElementById("table2"),
-    final: false,
-    calculate: document.getElementById("calculate")
-  }
-});
+new Calculator({
+	elements: {
+		original: document.getElementById("table1"),
+		new: document.getElementById("table2"),
+		final: false,
+		calculate: document.getElementById("calculate"),
+	}
+}); 
